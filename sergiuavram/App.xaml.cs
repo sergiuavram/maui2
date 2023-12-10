58b8d0fec5adcf09a14abcfe29a752be236b4fd2
sergiua <contact@sergiuavram.com>
2023-12-10 06:58:59 +0200

    lab8

--- sergiuavram/App.xaml.cs
+++ sergiuavram/App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace sergiuavram
+﻿using System;
+using sergiuavram.Data;
+using System.IO;
+
+namespace sergiuavram
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
